f46f61d324cbaa29629a5c6fcd52089a400a8e06
yhon 117 <jhonbaironrivera2004@gmail.com>
2024-06-15 02:29:24 -0500

    fucnion del carrito

--- src/main/java/com/yhon/ecommerce/controller/HomeController.java
+++ src/main/java/com/yhon/ecommerce/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.yhon.ecommerce.controller;
 
+import com.yhon.ecommerce.entitys.Producto;
 import com.yhon.ecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -23,8 +27,19 @@ public class HomeController {
     }
 
     @GetMapping("/productohome/{id}")
-    public String productoHome(@PathVariable Integer id){
+    public String productoHome(@PathVariable Integer id,Model model){
+
         System.out.println("id eviado como parametro: "+id);
+        Producto producto = new Producto();
+        Optional<Producto> productoOptional=productoService.get(id);
+        producto=productoOptional.get();
+
+        model.addAttribute("producto",producto);
         return "usuario/productohome";
+    }
+
+    @PostMapping("/cart")
+    public String carrito(){
+        return "usuario/carrito";
     }
 }
