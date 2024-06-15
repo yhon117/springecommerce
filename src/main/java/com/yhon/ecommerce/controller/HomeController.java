52915dac3a725f18dac93b1e430288d2b63cc042
yhon 117 <jhonbaironrivera2004@gmail.com>
2024-06-15 00:15:50 -0500

    se alladio la funcio para que el usurio vea los productos

---
+++ src/main/java/com/yhon/ecommerce/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.yhon.ecommerce.controller;
+
+import com.yhon.ecommerce.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping("")
+    public String home(Model model){
+
+        model.addAttribute("productos",productoService.finadAll());
+        return "usuario/home";
+    }
+}
