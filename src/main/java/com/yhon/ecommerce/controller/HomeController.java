d74dbf0ebde15c10820132cb50ff3ca50ddfcdae
yhon 117 <jhonbaironrivera2004@gmail.com>
2024-06-15 00:43:25 -0500

    se crero la vista de los productos al usurio

--- src/main/java/com/yhon/ecommerce/controller/HomeController.java
+++ src/main/java/com/yhon/ecommerce/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +20,11 @@ public class HomeController {
 
         model.addAttribute("productos",productoService.finadAll());
         return "usuario/home";
+    }
+
+    @GetMapping("/productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        System.out.println("id eviado como parametro: "+id);
+        return "usuario/productohome";
     }
 }
