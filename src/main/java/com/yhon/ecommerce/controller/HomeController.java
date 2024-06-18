75d1ad00f4d601009a4ee2a72a8f4913acdd1ca8
yhon 117 <jhonbaironrivera2004@gmail.com>
2024-06-18 01:25:54 -0500

    metodo para eliminar producto del carrito

--- src/main/java/com/yhon/ecommerce/controller/HomeController.java
+++ src/main/java/com/yhon/ecommerce/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.yhon.ecommerce.entitys.Orden;
 import com.yhon.ecommerce.entitys.Producto;
 import com.yhon.ecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -43,13 +44,52 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String carrito(@RequestParam Integer id, @RequestParam Integer cantidad){
+    public String carrito(@RequestParam Integer id, @RequestParam Integer cantidad,Model model){
         DetalleOrden detalleOrden = new DetalleOrden();
         Producto producto = new Producto();
         double sumaTotal=0;
         Optional<Producto> optionalProducto= productoService.get(id);
-        System.out.println("producto alladido: "+optionalProducto);
-        System.out.println("cantidad: "+cantidad);
+        producto=optionalProducto.get();
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio()*cantidad);
+        detalleOrden.setProducto(producto);
+
+        listaOrden.add(detalleOrden);
+
+        sumaTotal=listaOrden.stream().mapToDouble(dt-> dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart",listaOrden);
+        model.addAttribute("orden",orden);
+
+        return "usuario/carrito";
+    }
+
+    //quitar producto del carrito
+
+    @GetMapping("delete/cart/{id}")
+    public String eliminarProductoCarrito(@PathVariable Integer id, Model model){
+        List<DetalleOrden> newOrden= new ArrayList<>();
+
+        //se elimina el producto de la lista por el id
+        for(DetalleOrden detalleOrden:listaOrden){
+            if(detalleOrden.getProducto().getId()!=id){
+                newOrden.add(detalleOrden);
+            }
+        }
+
+        //se agrega la nueva lista
+        listaOrden=newOrden;
+
+        double sumaTotal=0;
+        sumaTotal=listaOrden.stream().mapToDouble(dt-> dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart",listaOrden);
+        model.addAttribute("orden",orden);
+
+
         return "usuario/carrito";
     }
 }
